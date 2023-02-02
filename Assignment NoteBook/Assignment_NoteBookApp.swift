fba47b59c50073f843e7e5c4bdc725d77cb8a5c4
Alex Anderson <andersonaj116@gmail.com>
2023-02-02 09:28:24 -0600

    Initial Commit

---
+++ Assignment NoteBook/Assignment_NoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NoteBookApp.swift
+//  Assignment NoteBook
+//
+//  Created by Alex Anderson on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
