4929d98e4d13db2d4b9af8225e55f4752c6f3ba1
Alex Anderson <andersonaj116@gmail.com>
2023-02-09 10:48:00 -0600

    Add Assignment Functionality

---
+++ Assignment NoteBook/AddAssignment.swift
@@ -0,0 +1,45 @@
+//
+//  AddAssignment.swift
+//  Assignment NoteBook
+//
+//  Created by Alex Anderson on 2/9/23.
+//
+
+import SwiftUI
+
+struct AddAssignment: View {
+    @ObservedObject var assignment: AssignmentNotebook
+    @State private var importance = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let importances = [ "Very Important", "Kinda Important", "Wait that was due 3 weeks ago?!"]
+    var body: some View {
+        NavigationView{
+            Form{
+                Picker("importance", selection: $importance){
+                    ForEach(Self.importances, id: \.self){
+                        importance in Text(importance)
+                    }
+                }
+                TextField("Description", text: $description)
+                DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
+            }
+            .navigationBarTitle("Add New Assignment", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Save"){
+                if importance.count > 0 && description.count > 0{
+                    let Assignment = Assignment(id: UUID(), importance: importance,
+                                        description: description, dueDate: dueDate)
+                    assignment.Assignments.append(Assignment)
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddAssignment(assignment: AssignmentNotebook())
+    }
+}
--- Assignment NoteBook/ContentView.swift
+++ Assignment NoteBook/ContentView.swift
@@ -8,19 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var assignmentNotebook = AssignmentNotebook()
+    @State private var showingAddAssignmentView = false
+    private var assignments:[Assignment] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List{
+                ForEach(assignments){
+                    Assignment in HStack{
+                        VStack(alignment: .leading){
+                            Text(Assignment.importance)
+                                .font(.headline)
+                            Text(Assignment.description)
+                        }
+                        Spacer()
+                        Text(Assignment.dueDate, style: .date)
+                    }
+                }
+                .onMove{indices, newOffset in assignmentNotebook.Assignments.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                    .onDelete{indexSet in assignmentNotebook.Assignments.remove(atOffsets: indexSet)}
+            }
+            .sheet(isPresented: $showingAddAssignmentView, content: {
+                AddAssignment(assignment: assignmentNotebook)
+            })
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(), trailing: Button(action:{
+                showingAddAssignmentView = true }){
+                  Image(systemName: "plus")
+                })
+            }
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Assignment: Identifiable, Codable{
+    var id = UUID()
+    var importance = String()
+    var description = String()
+    var dueDate = Date()
 }
